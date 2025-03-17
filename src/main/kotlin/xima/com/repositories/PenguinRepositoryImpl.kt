303e454630573c79f0f5b283e4fe1ad63a8ed538
gabriel-ximenes-telus <gabriel.ximenes@telus.com>
2025-03-17 14:19:02 -0300

    feat: refactoring repository

---
+++ src/main/kotlin/xima/com/repositories/PenguinRepositoryImpl.kt
@@ -0,0 +1,49 @@
+package xima.com.repositories
+
+import xima.com.data.model.Penguin
+
+private const val BASE_URL = "http://0.0.0.0:8080"
+
+class PenguinRepositoryImpl: PenguinRepository {
+    private val penguins = mutableListOf<Penguin>(
+        Penguin(1, "Joseph", "He's nice", "$BASE_URL/penguins/penguin1.jpg"),
+        Penguin(2, "Nikola", "He's nice two", "$BASE_URL/penguins/penguin2.jpg"),
+        Penguin(3, "Helena", "He's nice three", "$BASE_URL/penguins/penguin3.jpg"),
+        Penguin(4, "RG10", "He's nice fork", "$BASE_URL/penguins/penguin4.jpg"),
+        Penguin(5, "cassio", "He aint nice", "$BASE_URL/penguins/penguin5.jpg")
+    )
+
+    override fun getAllPenguins(name: String?): List<Penguin> {
+        return if (name != null) {
+            penguins.filter { it.name.equals(name, ignoreCase = true)}
+        } else {
+            penguins
+        }
+    }
+
+    override fun getPenguinById(id: String): Penguin? {
+        return penguins.find { it.id.toString() == id }
+    }
+
+    override fun getRandomPenguin(): Penguin {
+        return penguins.random()
+    }
+
+    override fun insertPenguin(penguin: Penguin) {
+        penguins.add(penguin)
+    }
+
+    override fun deletePenguinById(id: String): Boolean {
+        return penguins.removeIf { it.id.toString() == id }
+    }
+
+    override fun updatePenguinById(id: Int, updatedPenguin: Penguin): Boolean {
+        val penguin = penguins.firstOrNull() { it.id == id } ?: return false
+
+        penguin.description = updatedPenguin.description
+        penguin.name = updatedPenguin.name
+        penguin.id = updatedPenguin.id
+        penguin.imageUrl = updatedPenguin.imageUrl
+        return true
+    }
+}
